4a27f6bf9f3ac8b12e24c9690d07a03c70633fa6
Hussein Mahfouz <husseinmahfouz93@gmail.com>
2020-01-16 17:05:55 +0000

    Finished Practical One

--- Week 1: Spatial Interaction Models - Unconstrained/Spatial Interaction Modelling for Dummies.R
+++ Week 1: Spatial Interaction Models - Unconstrained/Spatial Interaction Modelling for Dummies.R
@@ -98,10 +98,10 @@ cdatasub <- dplyr::select(cdatasub, OrigCodeNew, DestCodeNew, Total, everything(
 # re-order so that 'lad15cd' is the first column in LondonBNG
 cdatasub <- dplyr::select(cdatasub, OrigCodeNew, DestCodeNew, Total, everything())
 
-# HUSSEIN
+# HUSSEIN 
 library(sf)
 LondonBNG_sf <- st_as_sf(LondonBNG)
-# re-order so that 'lad15cd' is the first column in LondonBNG
+# re-order so that 'lad15cd' is the first column in LondonBNG - OTHERWISE od2line WON'T WORK
 LondonBNG_sf <- dplyr::select(LondonBNG_sf, lad15cd, everything())
 # convert back to sp
 LondonBNG <- as(LondonBNG_sf, 'Spatial')
@@ -114,4 +114,136 @@ w <- cdatasub$Total / max(cdatasub$Total) * 10
 #now plot it...
 plot(travel_network, lwd = w)
 plot(LondonBNG, add=T)
+
+# leaflet map
+
+#transform to wgs84
+travel_networkwgs <- spTransform(travel_network, "+init=epsg:4326")
+#plot in leaflet
+leaflet() %>% addTiles() %>% addPolylines(data = travel_networkwgs)
+
+
+#now we can create pivot table to turn paired list into matrix (and compute the margins as well)
+cdatasubmat <- dcast(cdatasub, Orig ~ Dest, sum, value.var = "Total", margins=c("Orig", "Dest"))
+cdatasubmat
+
+# MODELLING
+
+#First plot the commuter flows against distance and then fit a model line with a ^-2 parameter
+# -2 is the parameter used for Newton's Gravity Model. We are just using it as a starting point
+plot1 <- qplot(cdata$dist, cdata$Total)
+#and now the model fit...
+plot1 + stat_function(fun=function(x)x^-2, geom="line", aes(colour="^-2"))
+
+#now, what about the origin and destination data...
+plot2 <- qplot(cdata$vi1_origpop, cdata$Total)
+plot2 + stat_function(fun=function(x)x^1, geom="line", aes(colour="^1"))
+
+plot3 <- qplot(cdata$wj2_destsal, cdata$Total)
+plot3 + stat_function(fun=function(x)x^1, geom="line", aes(colour="^1"))
+
+#OK, so it looks like we’re not far off (well, destination salary doesn’t look too promising as a predictor, 
+#but we’ll see how we get on…), so let’s see what flow estimates with these starting parameters look like.
+
+#set up some variables to hold our parameter values in:
+mu <- 1
+alpha <- 1
+beta <- -2
+k <- 1
+T2 <- sum(cdatasub$Total)
+
+#Now let’s create some flow estimates using Equation 2 above… Begin by applying the parameters to the variables:
+
+vi1_mu <- cdatasub$vi1_origpop^mu
+wj2_alpha <- cdatasub$wj2_destsal^alpha
+dist_beta <- cdatasub$dist^beta
+T1 <- vi1_mu*wj2_alpha*dist_beta
+k <- T2/sum(T1)
+
+#run the model and store all of the new flow estimates in a new column in the dataframe
+cdatasub$unconstrainedEst1 <- round(k*vi1_mu*wj2_alpha*dist_beta,0)
+#check that the sum of these estimates makes sense
+sum(cdatasub$unconstrainedEst1)
+
+#turn it into a little matrix and have a look at your handy work
+cdatasubmat1 <- dcast(cdatasub, Orig ~ Dest, sum, value.var = "unconstrainedEst1", margins=c("Orig", "Dest"))
+cdatasubmat1
+
+# How do the flow estimates compare with the actual flows? Eyeballing works, but we need something more mathy
+
+# TESTING THE “GOODNESS-OF-FIT”.
+
+# METHOD 1: R-Squared
+CalcRSquared <- function(observed,estimated){
+  r <- cor(observed,estimated)
+  R2 <- r^2
+  R2
+}
+
+CalcRSquared(cdatasub$Total,cdatasub$unconstrainedEst1)
+
+# our model accounts for about 51% of the variation of flows in the system. Not bad, but not brilliant either.
+
+# METHOD 2: RMSE
+
+CalcRMSE <- function(observed,estimated){
+  res <- (observed - estimated)^2
+  RMSE <- round(sqrt(mean(res)),3)
+  RMSE
+}
+
+CalcRMSE(cdatasub$Total,cdatasub$unconstrainedEst1)
+
+# The closer to 0 the RMSE value, the better the model. (Now it is 2503...let's try to improve it)
+
+# POISSON REGRESSION - To Calibrate
+
+# Flow distribution 
+qplot(cdata$Total) + geom_histogram()   # If it looks like Poisson, and it quacks like Poisson...
+
+qplot(log(dist), log(Total), data=cdata) + geom_smooth(method = lm)
+
+
+#run the unconstrained model
+uncosim <- glm(Total ~ log(vi1_origpop)+log(wj2_destsal)+log(dist), na.action = na.exclude, family = poisson(link = "log"), data = cdatasub)
+
+summary(uncosim) 
+
+# Calibrated values
+
+# k  (intercept) = -15.631802
+# μ = 1.747997
+# α = 1.642331
+# β = -1.411889
+
+# Calculate Flow Estimates Using Calibrated Coefficients
+
+#first asign the parameter values from the model to the appropriate variables
+k <- uncosim$coefficients[1]
+mu <- uncosim$coefficients[2]
+alpha <- uncosim$coefficients[3]
+beta <- -uncosim$coefficients[4]
+
+#now plug everything back into the Equation 6 model... (be careful with the positive and negative signing of the parameters as the beta parameter may not have been saved as negative so will need to force negative)
+cdatasub$unconstrainedEst2 <- exp(k+(mu*log(cdatasub$vi1_origpop))+(alpha*log(cdatasub$wj2_destsal))-(beta*log(cdatasub$dist)))
+
+#which is exactly the same as this...
+cdatasub$unconstrainedEst2 <- (exp(k)*exp(mu*log(cdatasub$vi1_origpop))*exp(alpha*log(cdatasub$wj2_destsal))*exp(-beta*log(cdatasub$dist)))
+
+#and of course, being R, there is an even easier way of doing this...
+cdatasub$fitted <- fitted(uncosim)
+
+#run the model and store all of the new flow estimates in a new column in the dataframe
+cdatasub$unconstrainedEst2 <- round(cdatasub$unconstrainedEst2,0)
+sum(cdatasub$unconstrainedEst2)
+
+#turn it into a little matrix and have a look at your handy work
+cdatasubmat2 <- dcast(cdatasub, Orig ~ Dest, sum, value.var = "unconstrainedEst2", margins=c("Orig", "Dest"))
+cdatasubmat2
+
+# And the $1,000,000 question - has calibrating the parameters improved the model…?
+CalcRSquared(cdatasub$Total,cdatasub$unconstrainedEst2)
+
+CalcRMSE(cdatasub$Total,cdatasub$unconstrainedEst2)
+
 
