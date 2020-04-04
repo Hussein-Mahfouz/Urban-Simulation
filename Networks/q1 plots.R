a7c890e076c80e3a0fb78ef3467f180c16089a7e
Hussein Mahfouz <husseinmahfouz93@gmail.com>
2020-04-04 17:42:49 +0100

    done

--- Networks/q1 plots.R
+++ Networks/q1 plots.R
@@ -7,27 +7,40 @@ library(ggthemes)
 inv_shortest_path_lon <- 
             gather(data=inv_shortest_path, key= Method, value=Result, 
                    Normalized_Betweeness_Iterative, Normalized_Betweeness_AAO, # columns to be gathered
-                   Normalized_Degree_Iterative, Normalized_Degree_AAO, Random_Removal,
+                   Normalized_Degree_Iterative, Normalized_Degree_AAO, 
+                   Normalized_Eigenvector_Iterative, Normalized_Eigenvector_AAO,
+                   Random_Removal,
                    factor_key = TRUE)
 
 gcc_lon <- gather(data=gcc, key= Method, value=Result, 
                   Betweeness_Iterative, Betweeness_AAO, # columns to be gathered
-                  Degree_Iterative, Degree_AAO, Random_Removal,
+                  Degree_Iterative, Degree_AAO, 
+                  Eigenvector_Iterative, Eigenvector_AAO, 
+                  Random_Removal,
                   factor_key = TRUE)
 
 # plot on same grid, colour argument groups by values in the column defined: here it is 'Method'
-ggplot(data=inv_shortest_path_lon,
-       aes(x=Perc_of_Nodes_Removed, y=Result, colour=Method)) +
-            geom_line(aes(linetype=Method, color=Method)) +      #linetype argument added to define types for different methods
-            scale_linetype_manual(values=c("solid", "dotted", "solid", "dotted", "dashed")) +
-            scale_color_manual(values=c('#E69F00','#E69F00', "#009E73", "#009E73", "#808080")) +
-            labs(y="Mean Inverse Shortest Path Length", x = "Nodes Removed (%)") +
-            theme_minimal()  # theme_fivethirtyeight()
+p <- ggplot(data=inv_shortest_path_lon,
+            aes(x=Perc_of_Nodes_Removed, y=Result, colour=Method)) +
+                 geom_line(aes(linetype=Method, color=Method)) +      #linetype argument added to define types for different methods
+                 scale_linetype_manual(values=c("solid", "dotted", "solid", "dotted", "solid", "dotted", "dashed")) +
+                 scale_color_manual(values=c('#E69F00','#E69F00', "#009E73", "#009E73", "#800020", "#800020", "#808080")) +
+                 labs(y="Mean Inverse Shortest Path Length", x = "Nodes Removed (%)") +
+                 ylim(0, 1) +   # because the eigenvector shoots up to infinity
+                 # theme_fivethirtyeight() 
+                 theme(legend.position="bottom", legend.text=element_text(size=rel(0.5))) 
+                 #theme_minimal(legend.position="bottom")  # theme_fivethirtyeight()
 
-ggplot(data=gcc_lon,
-       aes(x=Perc_of_Nodes_Removed, y=Result, colour=Method)) +
-            geom_line(aes(linetype=Method, color=Method)) +      #linetype argument added to define types for different methods
-            scale_linetype_manual(values=c("solid", "dotted", "solid", "dotted", "dashed")) +
-            scale_color_manual(values=c('#E69F00','#E69F00', "#009E73", "#009E73", "#808080")) +
-            labs(y="Size of GCC as Percentage of No. of Nodes", x = "Nodes Removed (%)") +
-            theme_minimal()  # theme_fivethirtyeight()
+
+ggsave(path = "Plots", file="Inv_shortest_path.png", p)
+
+p <- ggplot(data=gcc_lon,
+            aes(x=Perc_of_Nodes_Removed, y=Result, colour=Method)) +
+                 geom_line(aes(linetype=Method, color=Method)) +      #linetype argument added to define types for different methods
+                 scale_linetype_manual(values=c("solid", "dotted", "solid", "dotted", "solid", "dotted", "dashed")) +
+                 scale_color_manual(values=c('#E69F00','#E69F00', "#009E73", "#009E73", "#800020", "#800020", "#808080")) +
+                 labs(y="Size of GCC as Percentage of No. of Nodes", x = "Nodes Removed (%)") +
+                 theme_minimal()  # theme_fivethirtyeight()
+
+ggsave(path = "Plots", file="gcc.png", p)
+
--- Networks/q1.R
+++ Networks/q1.R
@@ -226,26 +226,72 @@ for (i in 1:gorder(g_london)){
 
 
 
-# EIGENVECTOR  (need to think about this)
+# EIGENVECTOR  
 
 # 1) iteratively 
 
-# # clone the graph so as not to ruin the original one
-# g_london_eigen <- g_london
-# # iterative deleting (delete and recalculate at each step)
-# for (i in 1:100){
-#   # get closeness
-#   eigen_london=eigen_centrality(g_london_eigen)  
-#   #save average value to dataframe
-#   inv_shortest_path[i,7] <- mean(is.finite(1/distances(g_london_eigen)))
-#   # sort tube stations by centrality measure value - descending order
-#   # convert to dataframe so as to extract names in next step
-#   sorted_eigen = sort(eigen_london$vector, decreasing = TRUE) %>%
-#     as.data.frame()
-#   # remove highest scoring vertex
-#   # row.names to get the tube station name
-#   g_london_eigen = delete.vertices(g_london_eigen, c(row.names(sorted_eigen)[1]))
-# }
+# clone the graph so as not to ruin the original one
+g_london_eigen <- g_london
+# iterative deleting (delete and recalculate at each step)
+for (i in 1:gorder(g_london)){
+  # get closeness
+  eig_london=eigen_centrality(g_london_eigen)  
+  # 1.get distance matrix
+  dist_mat <- distances(g_london_eigen, algorithm = "dijkstra")
+  # 2. get inverse of each shortest path
+  dist_inv <- 1/dist_mat
+  # 3. turn INF at the diagonal to 0s (if we do is.finite instead then diagonals aren't counted in denominator)
+  dist_inv[is.infinite(dist_inv)] <- 0
+  # 4. get number of vertex pairs (demonitaor = N(N-1))
+  ver_pair <- ((n_ver_init - i) * (n_ver_init - (i+1)))
+  # 5. store in results df mean(dist_inv) is the mean inverse shortest path
+  inv_shortest_path[i,15] <- sum(dist_inv) / ver_pair
+  # 6. normalize the results (divide by result obtained from first iteration)
+  inv_shortest_path[i,16] <- inv_shortest_path[i,15] / inv_shortest_path[1,15]
+  # gcc score
+  gcc[i,9] = (gorder(getgcc(g_london_eigen)))/(gorder(g_london))
+  # sort tube stations by centrality measure value - descending order
+  # convert to dataframe so as to extract names in next step
+  sorted_eigen = sort(eig_london[[1]], decreasing = TRUE) %>%
+    as.data.frame()
+  # remove highest scoring vertex
+  # row.names to get the tube station name
+  g_london_eigen = delete.vertices(g_london_eigen, c(row.names(sorted_eigen)[1]))
+}
+
+# 2) All at Once
+
+g_london_eigen <- g_london
+# get degree
+eig_london=eigen_centrality(g_london_eigen) 
+#sort and convert to dataframe so as to extract names in next step
+sorted_eigen = sort(eig_london[[1]], decreasing = TRUE) %>%
+  as.data.frame()
+
+for (i in 1:gorder(g_london)){
+  # get inverse shortest path and save value to dataframe
+  # 1.get distance matrix
+  dist_mat <- distances(g_london_eigen, algorithm = "dijkstra")
+  # 2. get inverse of each shortest path
+  dist_inv <- 1/dist_mat
+  # 3. turn INF at the diagonal to 0s (if we do is.finite instead then diagonals aren't counted in denominator)
+  dist_inv[is.infinite(dist_inv)] <- 0
+  # 4. get number of vertex pairs (demonitaor = N(N-1))
+  ver_pair <- ((n_ver_init - i) * (n_ver_init - (i+1)))
+  # 5. store in results df mean(dist_inv) is the mean inverse shortest path
+  inv_shortest_path[i,17] <- sum(dist_inv) / ver_pair
+  # 6. normalize the results (divide by result obtained from first iteration)
+  inv_shortest_path[i,18] <- inv_shortest_path[i,17] / inv_shortest_path[1,17]
+  # gcc score
+  gcc[i,10] = (gorder(getgcc(g_london_eigen))/(gorder(g_london)))
+  # remove highest scoring vertex
+  # row.names to get the tube station name
+  # use i instead of 1: we are not resorting so 1st tube station has been deleted and will raise an error in loop
+  g_london_eigen = delete.vertices(g_london_eigen, c(row.names(sorted_eigen)[i]))
+}
+
+
+
 
 
 # RENAME DF COLUMNS
@@ -264,6 +310,10 @@ names(inv_shortest_path)[11] <- "Degree_Iterative"
 names(inv_shortest_path)[12] <- "Normalized_Degree_Iterative"
 names(inv_shortest_path)[13] <- "Degree_AAO"
 names(inv_shortest_path)[14] <- "Normalized_Degree_AAO"
+names(inv_shortest_path)[15] <- "Eigenvector_Iterative"
+names(inv_shortest_path)[16] <- "Normalized_Eigenvector_Iterative"
+names(inv_shortest_path)[17] <- "Eigenvector_AAO"
+names(inv_shortest_path)[18] <- "Normalized_Eigenvector_AAO"
 
   # gcc df
 names(gcc)[2] <- "Perc_of_Nodes_Removed"
@@ -273,11 +323,6 @@ names(gcc)[5] <- "Closeness_Iterative"
 names(gcc)[6] <- "Closeness_AAO"
 names(gcc)[7] <- "Degree_Iterative"
 names(gcc)[8] <- "Degree_AAO"
+names(gcc)[9] <- "Eigenvector_Iterative"
+names(gcc)[10] <- "Eigenvector_AAO"
 
-
-
-# TO GET Largest Connected Component
-
-# getgcc() extracts the component
-# gorder() gets no of nodes/vertices
-# gorder(getgcc(graph)) ---> no of nodes in largest gcc
